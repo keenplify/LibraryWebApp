c68e97a3cbcb0341f5bf50dd3496b2d46a43a9d1
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2022-04-28 22:21:56 +0800

    created no nav

---
+++ LibraryWebApp/SiteNoNav.Master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using MySql.Data.MySqlClient;
+using System.Web;
+
+namespace LibraryWebApp
+{
+    public partial class SiteNoNav : MasterPage
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    protected void Logout(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
